9a8e4dae9e1a1418328ee7dec133783b813dec22
Francisco Abarca <francisco.abarcamo@sansano.usm.cl>
2019-09-30 03:20:33 -0300

    [To Do] Bug Checking

--- Cliente.py
+++ Cliente.py
@@ -5,13 +5,28 @@ PORT = 5000
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((SERVER,PORT))
-cliente.sendall(bytes("Este es el cliente", 'UTF-8'))
+entrada = cliente.recv(1024)
+print("[Headnode]", entrada.decode())
+cliente.sendall(bytes("Cliente", 'UTF-8'))
+
+def registrar(entrada):
+    """
+    Funcion para registrar donde se guardo el dato enviado al headnode\n
+    La entrada corresponde al mensaje del server
+    """
+    registro = open("Registro_Cliente.txt","a+")
+    registro.write(entrada + "\n")
+    registro.close()
 
 while True:
-    entrada = cliente.recv(1024)
-    print("Desde el servidor :", entrada.decode())
+    print("Ingrese su dato [escriba salir para terminar rutina]")
     salida = input()
-    cliente.sendall(bytes(salida, 'UTF-8'))
-    if salida == 'bye':
+    if salida == 'salir':
         break
+    else:
+        cliente.sendall(bytes(salida, 'UTF-8'))
+        entrada = cliente.recv(1024).decode()
+        print("[Headnode]", entrada)
+        registrar(entrada)
+cliente.sendall(bytes(salida, 'UTF-8'))
 cliente.close()
